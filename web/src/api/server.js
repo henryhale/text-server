73ea27e9c35985714f82b4011c504555b578b627
Henry Hale <92443116+henryhale@users.noreply.github.com>
2025-05-21 02:08:33 +0300

    chore: added readdir route

---
+++ web/src/api/server.js
@@ -0,0 +1,45 @@
+function objToFormData(obj) {
+    const fd = new FormData();
+
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            fd.append(key, obj[key]);
+        }
+    }
+
+    return fd;
+}
+
+const API_URL = `${window.location.origin}/api/file`;
+
+const apiRequest = (path, data) => {
+    return new Promise((res, rej) => {
+        fetch(API_URL + path, {
+            method: "POST",
+            body: objToFormData(data),
+        })
+            .then((r) => r.json())
+            .then(res)
+            .catch(rej);
+    });
+};
+
+const getRootTree = () => apiRequest("/root");
+
+const readdir = (path) => apiRequest("/list", { path });
+
+const readFile = (path) => apiRequest("/load", { path });
+
+const writeFile = (path, content) => apiRequest("/save", { path, content });
+
+const createFile = (path, name) => apiRequest("/create", { path, name });
+
+// const renameFile = (oldpath, newpath) => apiRequest("/rename", { oldpath, newpath });
+
+export default {
+    getRootTree,
+    readdir,
+    readFile,
+    writeFile,
+    createFile,
+};
