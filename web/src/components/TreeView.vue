5c7c4a5e97bbe113eaf460c00501e9f6f6e6b79b
Henry Hale <92443116+henryhale@users.noreply.github.com>
2026-01-11 15:50:47 +0300

    fix: allow file explorer to scroll when overflowing

--- web/src/components/TreeView.vue
+++ web/src/components/TreeView.vue
@@ -10,28 +10,23 @@ onBeforeMount(() => state.refreshExplorer());
 </script>
 
 <template>
-    <div>
-        <div
-            class="font-semibold text-sm px-3 py-2 border-b uppercase flex items-center space-x-1"
-        >
-            <div class="flex-grow">EXPLORER</div>
-            <button
-                @click.stop="state.refreshExplorer()"
-                title="Refresh Explorer"
-            >
-                <RotateCwIcon
-                    class="size-4"
-                    :class="{ 'animate-spin': state.app.loadingFiles }"
-                />
-            </button>
-        </div>
-        <ul class="p-1 select-none">
-            <TreeItem
-                v-for="item in state.app.files"
-                :key="item.path"
-                :item="item"
-                :autoload="true"
+    <div
+        class="font-semibold text-sm px-3 py-2 border-b uppercase flex items-center space-x-1"
+    >
+        <div class="flex-grow">EXPLORER</div>
+        <button @click.stop="state.refreshExplorer()" title="Refresh Explorer">
+            <RotateCwIcon
+                class="size-4"
+                :class="{ 'animate-spin': state.app.loadingFiles }"
             />
-        </ul>
+        </button>
     </div>
+    <ul class="p-1 select-none overflow-y-auto flex-grow">
+        <TreeItem
+            v-for="item in state.app.files"
+            :key="item.path"
+            :item="item"
+            :autoload="true"
+        />
+    </ul>
 </template>
